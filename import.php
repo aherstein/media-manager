7d6a1af2686f9cadb6d9dcbd284b477ede2c4c82
Adam Herstein <aherstein@Adam-Herstein-Work.local>
2014-05-13 15:17:00 -0500

    Added code to convert text line item to object.

--- import.php
+++ import.php
@@ -1,23 +1,17 @@
 <?php
+require_once("DataItem.php");
 
-class DataItem
+function lineToObject($lineText)
 {
-	public $stb;
-	public $title;
-	public $provider;
-	public $date;
-	public $rev;
-	public $viewTime;
+	$lineArray = explode("|", $lineText);
+	$dataObject = new DataItem(
+		lineArray[0], // stb
+		lineArray[1], // title
+		lineArray[2], // provider
+		lineArray[3], // date
+		lineArray[4], // rev
+		lineArray[5]  // viewTime
+	);
 
-	function __construct($stb = "", $title = "", $provider = "", $date = "", $rev = "", $viewTime = "")
-	{
-		$this->stb = $stb;
-		$this->title = $title;
-		$this->provider = $provider;
-		$this->date = $date;
-		$this->rev = $rev;
-		$this->viewTime = $viewTime;
-	}
-
-	
+	return $dataObject;
 }
